98280ac5dfee8d00fd9c35949fdc00e2239b9989
dceoy <dnarsil+github@gmail.com>
2019-05-15 18:20:50 +0900

    Add --only-ref-prep

--- bin/workflow.sh
+++ bin/workflow.sh
@@ -4,7 +4,7 @@
 #
 # Usage:
 #   workflow.sh --ref-gff=<path> --ref-fna=<path> [--in-dir=<path>]
-#               [--out-dir=<path>] [--qc] [--thread=<int>]
+#               [--out-dir=<path>] [--qc] [--only-ref-prep] [--thread=<int>]
 #   workflow.sh -h|--help
 #
 # Options:
@@ -13,6 +13,7 @@
 #   --in-dir=<path>   Path to input FASTQ directory [default: .]
 #   --out-dir=<path>  Path to output directory [default: .]
 #   --qc              Execute QC checks
+#   --only-ref-prep   Prepare only references
 #   --thread=<url>    Limit CPUs for multiprocessing
 #   -h, --help        Print usage
 
@@ -35,9 +36,10 @@ RSEM_TPM_SH="${BIN_DIR}/rsem_tpm.sh"
 
 REF_GFF=''
 REF_FNA=''
-IN_DIR='.'
-OUT_DIR='.'
+IN_DIR="${PWD}"
+OUT_DIR="${PWD}"
 QC=0
+ONLY_REF_PREP=0
 THREAD=1
 
 function print_version {
@@ -73,31 +75,34 @@ while [[ ${#} -ge 1 ]]; do
       shift 1
       ;;
     '--ref-gff' )
-      REF_GFF="${2}" && shift 2
+      REF_GFF=$(realpath "${2}") && shift 2
       ;;
     --ref-gff=* )
-      REF_GFF="${1#*\=}" && shift 1
+      REF_GFF=$(realpath "${1#*\=}") && shift 1
       ;;
     '--ref-fna' )
-      REF_FNA="${2}" && shift 2
+      REF_FNA=$(realpath "${2}") && shift 2
       ;;
     --ref-fna=* )
-      REF_FNA="${1#*\=}" && shift 1
+      REF_FNA=$(realpath "${1#*\=}") && shift 1
       ;;
     '--in-dir' )
-      IN_DIR="${2}" && shift 2
+      IN_DIR=$(realpath "${2}") && shift 2
       ;;
     --in-dir=* )
-      IN_DIR="${1#*\=}" && shift 1
+      IN_DIR=$(realpath "${1#*\=}") && shift 1
       ;;
     '--out-dir' )
-      OUT_DIR="${2}" && shift 2
+      OUT_DIR=$(realpath "${2}") && shift 2
       ;;
     --out-dir=* )
-      OUT_DIR="${1#*\=}" && shift 1
+      OUT_DIR=$(realpath "${1#*\=}") && shift 1
       ;;
     '--qc' )
       QC=1 && shift 1
+      ;;
+    '--only-ref-prep' )
+      ONLY_REF_PREP=1 && shift 1
       ;;
     '--thread' )
       THREAD="${2}" && shift 2
@@ -135,6 +140,17 @@ OUT_FQ_DIR="${OUT_DIR}/fq"
 OUT_REF_DIR="${OUT_DIR}/ref"
 OUT_MAP_DIR="${OUT_DIR}/map"
 
+FNA_NAME=$(basename "${REF_FNA}" | sed -e 's/\.[a-z]\+\.gz$//')
+OUT_REF_PREFIX="${OUT_REF_DIR}/${FNA_NAME}"
+if [[ -d "${OUT_REF_DIR}" ]]; then
+  echo ">>> STAR references exist: ${OUT_REF_PREFIX}"
+else
+  echo ">>> Prepare references using STAR and RSEM: ${OUT_REF_PREFIX}"
+  ${RSEM_REF_SH} \
+    "${REF_GFF}" "${REF_FNA}" "${OUT_REF_PREFIX}" "${THREAD}"
+fi
+[[ ${ONLY_REF_PREP} -eq 0 ]] || exit
+
 FQ_PREFIXES=$(find_fq_prefixes "${IN_DIR}")
 echo ">>> Input FASTQ samples:"
 echo "${FQ_PREFIXES}"
@@ -152,16 +168,6 @@ else
   echo ">>> Trim reads using PRINSEQ: ${IN_DIR} => ${OUT_FQ_DIR}"
   echo "${FQ_PREFIXES}" \
     | xargs -L 1 -P "${THREAD}" -t -i "${PRINSEQ_SH}" {} "${OUT_FQ_DIR}"
-fi
-
-FNA_NAME=$(basename "${REF_FNA}")
-OUT_REF_PREFIX="${OUT_REF_DIR}/${FNA_NAME#*\.}"
-if [[ -d "${OUT_REF_DIR}" ]]; then
-  echo ">>> Use existing STAR references: ${OUT_REF_PREFIX}"
-else
-  echo ">>> Prepare references using STAR and RSEM: ${OUT_REF_PREFIX}"
-  ${RSEM_REF_SH} \
-    "${REF_GFF}" "${REF_FNA}" "${OUT_REF_PREFIX}" "${THREAD}"
 fi
 
 for p in ${FQ_PREFIXES}; do
